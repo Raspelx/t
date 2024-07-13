2e196c315f82cfd346b74eebfb02a1cb917d2b7f
Raspelx <135234365+Raspelx@users.noreply.github.com>
2024-07-13 22:29:12 +0300

    Update t.lua

--- t.lua
+++ t.lua
@@ -96,7 +96,7 @@ getgenv().ExunysDeveloperAimbot = {
 
 		TeamCheck = false,
 		AliveCheck = true,
-		WallCheck = true,
+		WallCheck = false,
 
 		OffsetToMoveDirection = true,
 		OffsetIncrement = 15,
